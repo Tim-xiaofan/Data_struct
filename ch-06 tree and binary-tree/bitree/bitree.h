6f40d2758ab7a0647d3de67c238eed47be453633
zyj <zhongyinjiebit@sina.com>
2021-04-19 17:15:22 +0800

    bitree:finished create and traverse

--- ch-06 tree and binary-tree/bitree/bitree.h
+++ ch-06 tree and binary-tree/bitree/bitree.h
@@ -3,8 +3,13 @@
 #ifndef BITREE_H
 #define BITREE_H
 #include <iostream>
+#include "sqstack.h"
 using std::cout;
 using std::endl;
+
+
+template<typename Data>
+void show_ds(const Data * ds, int ct);
 
 template <typename Data>
 class bitree
@@ -32,8 +37,10 @@ class bitree
 		static int less(const Data & d1, const Data & d2){return d1 - d2;}
 		static void show(const Data & d){cout << d <<" ";}
 		bool preinorder_construct(node * & root, const Data * pre, const Data *in, int ct);
+		template<typename UnaryOperator>
+		void inorder_traversex(node * root, const UnaryOperator & op);
 	public:
-		bitree():_root(nullptr), _ct(0){cout << "diy\n";};
+		bitree():_root(nullptr), _ct(0){}
 		//template<typename UnaryOperator>
 		//void test(const UnaryOperator & op);
 		/** root fisrt*/
@@ -45,8 +52,11 @@ class bitree
 		/** cmp construct*/
 		template<typename Cmp = int(*)(const Data &, const Data&)>
 		bool cmp_construct(const Data * ds, int ct, const Cmp & cmp = less);
-		/** preorder and inorder creat*/
+		/** preorder and inorder create*/
 		bool preinorder_construct(const Data * pre, const Data *in, int ct){return preinorder_construct(_root, pre, in , ct);}
+		/** root second, not recursion*/
+		template<typename UnaryOperator = void(*)(const Data &)>
+		void inorder_traversex(const UnaryOperator & op = show){inorder_traversex(_root, op); cout << endl;}
 };
 
 /** root first*/
@@ -62,6 +72,22 @@ preorder_traverse(node *root, const UnaryOperator & op)
 		preorder_traverse(root->lchild, op);
 		/** traverse right child*/
 		preorder_traverse(root->rchild, op);
+	}
+}
+
+/** root second*/
+template<typename Data>
+template<typename UnaryOperator>
+void bitree<Data>::
+inorder_traverse(node *root, const UnaryOperator & op)
+{
+	if(root)
+	{
+		/** traverse left child*/
+		inorder_traverse(root->lchild, op);
+		op(root->data);
+		/** traverse right child*/
+		inorder_traverse(root->rchild, op);
 	}
 }
 
@@ -116,22 +142,65 @@ template<typename Data>
 bool bitree<Data>:: 
 preinorder_construct(node * & root, const Data * pre, const Data *in, int ct)
 {
-	if(pre == nullptr || in = nullptr || ct == 0)
+	if(pre == nullptr || in == nullptr || ct <= 0)
 	  return true;
+
+	//cout << "pre:";
+	//show_ds(pre, ct);
+	//cout << "in:";
+	//show_ds(in, ct);
 	/** create root*/
 	if(root == nullptr) root = new node;
 	root->data = pre[0]; 
+	//cout << "root data = " << root->data << endl;
 
-	/** look left and right*/
-	int lct = 0, rct = ct, root_pos;
+	/** look left and right in inorder*/
+	int lct = 0, rct = 0, root_pos;
 	for(root_pos = 0; root_pos < ct; ++root_pos)
-	{
-		++lct;
-		--rct;
-		if(in[root_pos] == root->data)
-			break;
-	}
+	  if(in[root_pos] == root->data)
+		break;
+	lct = root_pos;
+	rct = ct - lct - 1;
+	//cout << "lct = " << lct << endl;
+	//cout << "rct = " << rct << endl;
 
-	return false;
+	/** create lchild*/
+	preinorder_construct(root->lchild, &pre[1], in, lct);
+	/** create rchild*/
+	preinorder_construct(root->rchild, &pre[1 + lct], &in[lct + 1], rct);
+	return true;
+}
+
+template<typename Data>
+void show_ds(const Data * ds, int ct)
+{
+	int i;
+	for(i = 0; i < ct; ++i)
+	  cout << ds[i] << " ";
+	cout << endl;
+}
+
+/** root second, not recursion*/
+template<typename Data>
+template<typename UnaryOperator>
+void bitree<Data>::
+inorder_traversex(node *root, const UnaryOperator & op)
+{
+	sqstack<node *> stack(32);
+	node * pn = root;
+	while(!stack.is_empty() || pn)
+	{
+		if(pn)
+		{
+			stack.push(pn);
+			pn = pn->lchild;
+		}
+		else
+		{
+			stack.pop(pn);
+			op(pn->data);
+			pn = pn->rchild;
+		}
+	}
 }
 #endif
--- ch-06 tree and binary-tree/bitree/test.cpp
+++ ch-06 tree and binary-tree/bitree/test.cpp
@@ -8,21 +8,31 @@
 using std::cout;
 using std::endl;
 
-struct G
+template <typename Data>
+struct show
 {
-    void operator()(char a) const {cout << a <<" functor\n";}
+    void operator()(const Data & a) const {cout << a <<" ";}
 };
 
-void f(char a) {cout << a <<" funtion \n";}
-void show(int a) {cout << a << " ";}
-
 int elements[5]={2,4,5,1,3};
+int preorder[5] = {3,9,20,15,7};
+int inorder[5] = {9,3,15,20,7};
+char preorder1[12] = "-+a*b-cd/ef";
+char inorder1[12] = "a+b*c-d-e/f";
+
+#define SEP(info) cout << "-----------" << (info) << "-----------\n"
 
 int main()
 {
+	SEP("int");
 	bitree<int> tree;
-	tree.cmp_construct(elements, 5);
+	tree.preinorder_construct(preorder, inorder, 5);
 	cout << "preorder show : ";
-	tree.preorder_traverse(show);
+	tree.preorder_traverse(show<int>());
+	cout << "inorder show : ";
+	tree.inorder_traverse(show<int>());
+	cout << "inorderx show : ";
+	tree.inorder_traversex(show<int>());
+
 	return 0;
 }
