dc790b6f9ff9b680d401ac2a3eaee61483422d81
zyj <zhongyinjiebit@sina.com>
2021-08-10 18:23:56 +0800

    有向图图：十字链表

---
+++ ch-07 graph/graph-olist/graph_olist.h
@@ -0,0 +1,22 @@
+/** 20210810 12:35, zyj, GuangDong */
+#ifndef _GRAPH_OLIST_H
+#define _GRAPH_OLIST_H
+#include <iostream>
+#include <iomanip>
+#include <cstdarg>
+#include <climits>
+#include <cfloat>
+#include <new>
+#include <typeinfo>
+#include "array.h"
+#include "List.h"
+using std::endl;
+using std::cout;
+using std::cerr;
+
+template <typename T, typename U>
+class graph_olist
+{
+};
+
+#endif
---
+++ ch-07 graph/graph-olist/test.cpp
@@ -0,0 +1,59 @@
+/** 20210810 12:35, zyj, GuangDong*/
+//test.c -- test graph_array.h
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <cfloat>
+#include "graph_olist.h"
+
+using std::cout;
+using std::endl;
+using std::cerr;
+typedef array<int, 2> a2;
+typedef array<char, 1> a1;
+
+static int a[]={
+	0,1,1,0,
+	0,0,0,0,
+	0,0,0,1,
+	1,0,0,0
+};
+
+static int c[]={
+	0,1,0,1,0,
+	1,0,1,0,1,
+	0,1,0,1,1,
+	1,0,1,0,0,
+	0,1,1,0,0
+};
+
+static char b[] = {'A', 'B', 'C', 'D'};
+static char d[] = {'A', 'B', 'C', 'D', 'E'};
+
+int main(int ac, char * av[])
+{
+	a1 * vexs = a1::instance(4);
+	if(!vexs) 
+	{
+		cerr << "vexs create error\n";
+		exit(EXIT_FAILURE);
+	}
+	vexs->set_values(b, 4);
+	a2 * arcs = a2::instance(4, 4);
+	if(!arcs) 
+	{
+		cerr << "arcs create error\n";
+		exit(EXIT_FAILURE);
+	}
+	arcs->set_values(a, 16);
+
+	delete vexs;
+	delete arcs;
+	vexs = a1::instance(5);
+	vexs->set_values(d, 5);
+	arcs = a2::instance(5, 5);
+	arcs->set_values(c, 25);
+	return 0;
+}
+
