4885f733c601d5cf54dfa5682f2431ee6be092a8
zyj <1305696765@qq.com>
2021-08-11 22:12:33 +0800

    DFS

---
+++ ch-07 graph/DFS/DFS.h
@@ -0,0 +1,24 @@
+/** 20210811 22:00, zyj, GuangDong */
+#ifndef _DFS_H
+#define _DFS_H
+#include <iostream>
+#include <iomanip>
+#include <cstdarg>
+#include <climits>
+#include <cfloat>
+#include <new>
+#include <typeinfo>
+#include "array.h"
+#include "List.h"
+#include "graph_AML.h"
+
+using std::endl;
+using std::cout;
+using std::cerr;
+
+template <typename T, typename U>
+void DFS(const graph_AML<T, U> & G)
+{
+}
+
+#endif
