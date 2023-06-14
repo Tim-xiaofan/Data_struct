9c83ce017531e27b99c3e43ad077eb500f90e30e
zyj <zhongyinjiebit@sina.com>
2023-06-14 10:43:23 +0800

    动态查找::AVL树::插入

--- homework/ch-07 search/AVLTree.cc
+++ homework/ch-07 search/AVLTree.cc
@@ -42,7 +42,6 @@ int main(void)
 
 		assert(root->same(expected));
 	}
-
 	//test leftRotate
 	{
 		AVLTree<int> root = new AVLTreeNode<int>(1);
@@ -58,6 +57,93 @@ int main(void)
 		lchild->addLchild(0);
 		lchild->addRchild(2);
 		expected->addRchild(4);
+		assert(root->same(expected));
+	}
+
+	//test leftBalance
+	{//case 1: LL
+		AVLTree<int> A = new AVLTreeNode<int>(4, 2);
+		AVLTree<int> B = A->addLchild(2, 1);
+		B->addLchild(1, 1)->addLchild(0, 0);;
+		B->addRchild(3, 0);
+		A->addRchild(5, 0);
+
+		leftBalance(A);
+
+		AVLTree<int> expected = new AVLTreeNode<int>(2, 0);
+		expected->addLchild(1, 1)->addLchild(0, 0);;
+		expected->addRchild(4, 0)->addRchild(5, 0);;
+		expected->rchild->addLchild(3, 0);
+
+		assert(A->same(expected));
+	}
+	{//case 2: LR
+		AVLTree<int> A = new AVLTreeNode<int>(4, 2);
+		AVLTree<int> B = A->addLchild(1, -1);
+		B->addLchild(0, 0);
+		B->addRchild(3, 1)->addLchild(2, 0);
+		A->addRchild(5, 0);
+
+		leftBalance(A);
+
+		AVLTree<int> expected = new AVLTreeNode<int>(3, 0);
+		expected->addLchild(1, 0)->addLchild(0, 0);
+		expected->lchild->addRchild(2, 0);
+		expected->addRchild(4, -1)->addRchild(5, 0);
+
+		assert(A->same(expected));
+	}
+
+	//test rightBalance
+	{//case RR
+		AVLTree<int> root = new AVLTreeNode<int>(1, -2);
+		AVLTree<int> rchild = root->addRchild(3, -1);
+		rchild->addRchild(4, -1)->addRchild(5, 0);
+		rchild->addLchild(2, 0);
+		root->addLchild(0, 0);
+
+		rightBalance(root);
+
+		AVLTree<int> expected = new AVLTreeNode<int>(3, 0);
+		AVLTree<int> lchild = expected->addLchild(1, 0);
+		lchild->addLchild(0, 0);
+		lchild->addRchild(2, 0);
+		expected->addRchild(4, -1)->addRchild(5, 0);
+
+		assert(root->same(expected));
+	}
+	{//RL
+		AVLTree<int> root = new AVLTreeNode<int>(1, -2);
+		AVLTree<int> rchild = root->addRchild(4, 1);
+		rchild->addLchild(3, 1)->addLchild(2, 0);
+		rchild->addRchild(5, 0);
+		root->addLchild(0, 0);
+
+		rightBalance(root);
+
+		AVLTree<int> expected = new AVLTreeNode<int>(3, 0);
+		expected->addRchild(4, -1)->addRchild(5, 0);
+		expected->addLchild(1, 0)->addLchild(0, 0);
+		expected->lchild->addRchild(2, 0);
+
+		assert(root->same(expected));
+	}
+
+	//test insert
+	{
+		AVLTree<int> root = nullptr;
+		std::vector<int> table = {13, 24, 37, 90, 53};
+		for(int i: table)
+		{
+			bool taller;
+			assert(insert(root, i, taller));
+		}
+
+		AVLTree<int> expected = new AVLTreeNode<int>(24, -1);
+		expected->addRchild(53, 0)->addRchild(90, 0);
+		expected->rchild->addLchild(37, 0);
+		expected->addLchild(13, 0);
+
 		assert(root->same(expected));
 	}
 
--- homework/ch-07 search/AVLTree.h
+++ homework/ch-07 search/AVLTree.h
@@ -2,12 +2,16 @@
 #define _AVLTREE_H_
 
 #include <utility>
+#include <stdexcept>
 
+enum{LH=1, EH=0, RH=-1};
 
 template<typename T>
 struct AVLTreeNode
 {
 	using AVLTree = AVLTreeNode*;
+
+
 	T data;
 	int factor;
 	AVLTree lchild;
@@ -20,30 +24,30 @@ struct AVLTreeNode
 		rchild(nullptr)
 	{}
 
-	AVLTreeNode(const T& d):
+	AVLTreeNode(const T& d, int f=0):
 		data(d),
-		factor(0),
+		factor(f),
 		lchild(nullptr),
 		rchild(nullptr)
 	{}
 
-	AVLTree addLchild(const T& d)
+	AVLTree addLchild(const T& d, int f=0)
 	{
 		assert(!lchild);
-		lchild = new AVLTreeNode(d);
+		lchild = new AVLTreeNode(d, f);
 		return lchild;
 	}
 
-	AVLTree addRchild(const T& d)
+	AVLTree addRchild(const T& d, int f=0)
 	{
 		assert(!rchild);
-		rchild = new AVLTreeNode(d);
+		rchild = new AVLTreeNode(d, f);
 		return rchild;
 	}
 
 	bool same(const AVLTree other) const
 	{
-		if(other->data != other->data) return false;
+		if(data != other->data && factor == other->factor) return false;
 		if((lchild == nullptr) != (other->lchild == nullptr)) return false; 
 		if((rchild == nullptr) != (other->rchild == nullptr)) return false; 
 
@@ -86,7 +90,7 @@ struct AVLTreeNode
 		for (int i = 0; i < level; ++i) {
 			std::cout << "   ";
 		}
-		std::cout << "|_" << data << std::endl;
+		std::cout << "|_<" << data << "," << factor <<  ">\n";
 		if (lchild) {
 			lchild->display(level + 1);
 		}
@@ -95,9 +99,7 @@ struct AVLTreeNode
 		}
 	}
 
-	
 };
-
 
 template<typename T>
 using AVLTree = AVLTreeNode<T>*;
@@ -118,6 +120,146 @@ void leftRotate(AVLTree<T>& B)
 	B->rchild = C->lchild;
 	C->lchild = B;
 	B = C;
+}
+
+template<typename T>
+void leftBalance(AVLTree<T>& A)
+{
+	AVLTree<T>& B = A->lchild;
+	AVLTree<T>& C = B->rchild;
+	switch(B->factor)
+	{
+		case LH: //LL
+			A->factor = EH;
+			B->factor = EH;
+			rightRotate(A);
+			break;
+
+		case RH: //LR
+			switch (C->factor)
+			{
+				case LH:
+					A->factor = RH;
+					B->factor = EH;
+					break;
+				case EH:
+					A->factor = EH;
+					B->factor = EH;
+					break;
+				case RH:
+					A->factor = EH;
+					B->factor = LH;
+					break;
+			}
+			C->factor = EH;
+			leftRotate(B);
+			rightRotate(A);
+			break;
+
+		default:
+			throw std::logic_error("Must be LL or LR");
+			break;
+	}
+}
+
+template<typename T>
+void rightBalance(AVLTree<T>& A)
+{
+	AVLTree<T>& B = A->rchild;
+	AVLTree<T>& C = B->lchild;
+	switch(B->factor)
+	{
+		case RH: //RR
+			A->factor = EH;
+			B->factor = EH;
+			leftRotate(A);
+			break;
+
+		case LH: //RL
+			switch (C->factor)
+			{
+				case LH:
+					A->factor = EH;
+					B->factor = RH;
+					break;
+				case EH:
+					A->factor = EH;
+					B->factor = EH;
+					break;
+				case RH:
+					A->factor = LH;
+					B->factor = EH;
+					break;
+			}
+			C->factor = EH;
+			rightRotate(B);
+			leftRotate(A);
+			break;
+
+		default:
+			throw std::logic_error("Must be RR or RL");
+			break;
+	}
+}
+
+template<typename T>
+bool insert(AVLTree<T>& A, const T& value, bool& taller)
+{
+	if(!A)
+	{
+		A = new AVLTreeNode<T>(value, EH);
+		taller = true;
+		return true;
+	}
+	
+	taller = false;
+	if(value < A->data)
+	{
+		if(!insert(A->lchild, value, taller)) return false;
+		if(taller)
+		{
+			switch(A->factor)
+			{
+				case LH:
+					leftBalance(A);
+					taller = false;
+					break;
+				case EH:
+					A->factor = LH;
+					taller = true;
+					break;
+				case RH:
+					A->factor = EH;
+					taller = false;
+					break;
+			}
+		}
+		return true;
+	}
+	else if(value > A->data)
+	{
+		if(!insert(A->rchild, value, taller)) return false;
+		if(taller)
+		{
+			switch(A->factor)
+			{
+				case RH:
+					rightBalance(A);
+					taller = false;
+					break;
+				case EH:
+					A->factor = RH;
+					taller = true;
+					break;
+				case LH:
+					A->factor = EH;
+					taller = false;
+					break;
+			}
+		}
+		return true;
+	}
+	return false;
 }
 
 #endif
