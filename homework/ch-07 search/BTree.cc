e93e789ff68f95eb594d09134c9b3b1b6174c3eb
zyj <1305696765@qq.com>
2023-06-19 21:49:50 +0800

    动态查找::B树::查找

---
+++ homework/ch-07 search/BTree.cc
@@ -0,0 +1,310 @@
+#include <iostream>
+#include <cassert>
+#include "BTree.h"
+
+static void testBTreeNode()
+{
+	// getKeyInsertIndex
+	{// empty
+		BTreeNode<int> n;
+		assert(n.getKeyInsertIndex(12) == 0);
+	}
+	{// one
+		BTreeNode<int> n;
+		n.keys = std::vector<int>({0});
+		assert(n.getKeyInsertIndex(-1) == 0);
+		assert(n.getKeyInsertIndex(1) == 1);
+		assert(n.getKeyInsertIndex(2) == 1);
+	}
+	{//two
+		BTreeNode<int> n;
+		n.keys = std::vector<int>({0, 2});
+		assert(n.getKeyInsertIndex(-1) == 0);
+		assert(n.getKeyInsertIndex(1) == 1);
+		assert(n.getKeyInsertIndex(5) == 2);
+	}
+	{// three
+		BTreeNode<int> n;
+		n.keys = std::vector<int>({0, 2, 4});
+		assert(n.getKeyInsertIndex(-1) == 0);
+		assert(n.getKeyInsertIndex(1) == 1);
+		assert(n.getKeyInsertIndex(3) == 2);
+		assert(n.getKeyInsertIndex(5) == 3);
+	}
+	{//four
+		BTreeNode<int> n;
+		n.keys = std::vector<int>({0, 2, 4, 6});
+		assert(n.getKeyInsertIndex(-1) == 0);
+		assert(n.getKeyInsertIndex(1) == 1);
+		assert(n.getKeyInsertIndex(3) == 2);
+		assert(n.getKeyInsertIndex(5) == 3);
+		assert(n.getKeyInsertIndex(7) == 4);
+	}
+
+	//test insertKey
+	{//empty
+		BTreeNode<int> n;
+		n.insertKey(0, 1);
+		n.insertKey(1, 2);
+		assert(n.keys == std::vector<int>({1, 2}));
+
+		assert(n.findKey(1) == std::make_pair(true, 0));
+		assert(n.findKey(2) == std::make_pair(true, 1));
+		assert(n.findKey(3) == std::make_pair(false, 2));
+	}
+	{//one
+		BTreeNode<int> n;
+		n.keys = std::vector<int>({0});
+		n.insertKey(0, 1);
+		n.insertKey(2, 2);
+		n.insertKey(1, 3);
+		assert(n.keys == std::vector<int>({1, 3, 0, 2}));
+	}
+
+	// test insertChild
+	{
+		BTreeNode<int> n;
+		BTreeNode<int>* p0 = new BTreeNode<int>();
+		BTreeNode<int>* p1 = new BTreeNode<int>();
+		n.insertChild(0, p0);
+		n.insertChild(1, p1);
+		assert(n.children == std::vector<BTreeNode<int>*>({p0, p1}));
+	}
+
+	// test spilt
+	{// m = 3
+		const int m = 3;
+		BTreeNode<int> n;
+		n.insertKey(0, 0);
+		n.insertKey(1, 1);
+		n.insertKey(2, 2);
+		BTreeNode<int>* p0 = new BTreeNode<int>();
+		BTreeNode<int>* p1 = new BTreeNode<int>();
+		BTreeNode<int>* p2 = new BTreeNode<int>();
+		BTreeNode<int>* p3 = new BTreeNode<int>();
+		n.insertChild(0, p0);
+		n.insertChild(1, p1);
+		n.insertChild(2, p2);
+		n.insertChild(3, p3);
+
+		auto r = n.split((m-1)/2);
+		assert(n.keys == std::vector<int>({0}));
+		assert(r->keys == std::vector<int>({2}));
+		assert(n.children == std::vector<BTreeNode<int>*>({p0, p1}));
+		assert(r->children == std::vector<BTreeNode<int>*>({p2, p3}));
+	}
+	{// m = 4
+		const int m = 4;
+		BTreeNode<int> n;
+		n.insertKey(0, 0);
+		n.insertKey(1, 1);
+		n.insertKey(2, 2);
+		n.insertKey(3, 3);
+		BTreeNode<int>* p0 = new BTreeNode<int>();
+		BTreeNode<int>* p1 = new BTreeNode<int>();
+		BTreeNode<int>* p2 = new BTreeNode<int>();
+		BTreeNode<int>* p3 = new BTreeNode<int>();
+		BTreeNode<int>* p4 = new BTreeNode<int>();
+		n.insertChild(0, p0);
+		n.insertChild(1, p1);
+		n.insertChild(2, p2);
+		n.insertChild(3, p3);
+		n.insertChild(4, p4);
+
+		auto r = n.split((m-1)/2);
+		assert(n.keys == std::vector<int>({0}));
+		assert(r->keys == std::vector<int>({2, 3}));
+		assert(n.children == std::vector<BTreeNode<int>*>({p0, p1}));
+		assert(r->children == std::vector<BTreeNode<int>*>({p2, p3, p4}));
+	}
+	{// m = 5
+		const int m = 5;
+		BTreeNode<int> n;
+		n.insertKey(0, 0);
+		n.insertKey(1, 1);
+		n.insertKey(2, 2);
+		n.insertKey(3, 3);
+		n.insertKey(4, 4);
+		BTreeNode<int>* p0 = new BTreeNode<int>();
+		BTreeNode<int>* p1 = new BTreeNode<int>();
+		BTreeNode<int>* p2 = new BTreeNode<int>();
+		BTreeNode<int>* p3 = new BTreeNode<int>();
+		BTreeNode<int>* p4 = new BTreeNode<int>();
+		BTreeNode<int>* p5 = new BTreeNode<int>();
+		n.insertChild(0, p0);
+		n.insertChild(1, p1);
+		n.insertChild(2, p2);
+		n.insertChild(3, p3);
+		n.insertChild(4, p4);
+		n.insertChild(5, p5);
+
+		auto r = n.split((m-1)/2);
+		assert(n.keys == std::vector<int>({0, 1}));
+		assert(r->keys == std::vector<int>({3, 4}));
+		assert(n.children == std::vector<BTreeNode<int>*>({p0, p1, p2}));
+		assert(r->children == std::vector<BTreeNode<int>*>({p3, p4, p5}));
+	}
+
+	
+	std::cout << "All BTreeNode tests passed\n";
+}
+
+static void
+testBTree(void)
+{
+	//test search
+	{// one
+		BTree<int, 3> t;
+		auto& root = t.root();
+		root = new BTreeNode<int>();
+		root->insertKey(0, 45);
+		root->insertChild(0, nullptr);
+		root->insertChild(1, nullptr);
+
+		{
+			BTree<int, 3>::Result expected(true, root, 0);
+			auto ret = t.search(45);
+			assert(ret == expected);
+		}
+		{
+			BTree<int, 3>::Result expected(false, root, 1);
+			auto ret = t.search(46);
+			assert(ret == expected);
+		}
+	}
+	{// three
+		BTreeNode<int>* p0 = new BTreeNode<int>();
+		p0->insertKey(0, 24);
+		p0->insertChild(0, nullptr);
+		p0->insertChild(1, nullptr);
+
+		BTreeNode<int>* p1 = new BTreeNode<int>();
+		p1->insertKey(0, 53);
+		p1->insertKey(1, 90);
+		p1->insertChild(0, nullptr);
+		p1->insertChild(1, nullptr);
+		p1->insertChild(2, nullptr);
+
+		BTree<int, 3> t;
+		auto& root = t.root();
+		root = new BTreeNode<int>();
+		root->insertKey(0, 45);
+		root->insertChild(0, p0);
+		root->insertChild(1, p1);
+		{//in root
+			BTree<int, 3>::Result expected(true, root, 0);
+			auto ret = t.search(45);
+			assert(ret == expected);
+		}
+		{// not in p1
+			BTree<int, 3>::Result expected(false, p1, 0);
+			auto ret = t.search(46);
+			assert(ret == expected);
+		}
+		{// not in p1
+			BTree<int, 3>::Result expected(false, p1, 1);
+			auto ret = t.search(60);
+			assert(ret == expected);
+		}
+		{// not in p1
+			BTree<int, 3>::Result expected(false, p1, 2);
+			auto ret = t.search(95);
+			assert(ret == expected);
+		}
+		{//in p1
+			BTree<int, 3>::Result expected(true, p1, 0);
+			auto ret = t.search(53);
+			assert(ret == expected);
+		}
+		{// in p1
+			BTree<int, 3>::Result expected(true, p1, 1);
+			auto ret = t.search(90);
+			assert(ret == expected);
+		}
+		{// not in p0
+			BTree<int, 3>::Result expected(false, p0, 0);
+			auto ret = t.search(3);
+			assert(ret == expected);
+		}
+		{// not in p0
+			BTree<int, 3>::Result expected(false, p0, 1);
+			auto ret = t.search(25);
+			assert(ret == expected);
+		}
+		{// in p0
+			BTree<int, 3>::Result expected(true, p0, 0);
+			auto ret = t.search(24);
+			assert(ret == expected);
+		}
+	}
+	{//eight
+		BTreeNode<int>* p0 = new BTreeNode<int>();
+		p0->insertKey(0, 3);
+		p0->insertKey(1, 12);
+		p0->insertChild(0, nullptr);
+		p0->insertChild(1, nullptr);
+		p0->insertChild(2, nullptr);
+
+		BTreeNode<int>* p1 = new BTreeNode<int>();
+		p1->insertKey(0, 37);
+		p1->insertChild(0, nullptr);
+		p1->insertChild(1, nullptr);
+
+		BTreeNode<int>* p2 = new BTreeNode<int>();
+		p2->insertKey(0, 50);
+		p2->insertChild(0, nullptr);
+		p2->insertChild(1, nullptr);
+
+		BTreeNode<int>* p3 = new BTreeNode<int>();
+		p3->insertKey(0, 61);
+		p3->insertKey(1, 70);
+		p3->insertChild(0, nullptr);
+		p3->insertChild(1, nullptr);
+		p3->insertChild(2, nullptr);
+
+
+		BTreeNode<int>* p4 = new BTreeNode<int>();
+		p4->insertKey(0, 100);
+		p4->insertChild(0, nullptr);
+		p4->insertChild(1, nullptr);
+
+
+		BTreeNode<int>* p5 = new BTreeNode<int>();
+		p5->insertKey(0, 24);
+		p5->insertChild(0, p0);
+		p5->insertChild(1, p1);
+
+
+		BTreeNode<int>* p6 = new BTreeNode<int>();
+		p6->insertKey(0, 53);
+		p6->insertKey(1, 90);
+		p6->insertChild(0, p2);
+		p6->insertChild(1, p3);
+		p6->insertChild(2, p4);
+
+		BTree<int, 3> t;
+		t.root() = new BTreeNode<int>();
+		t.root()->insertKey(0, 45);
+		t.root()->insertChild(0, p5);
+		t.root()->insertChild(1, p6);
+
+		{
+			BTree<int, 3>::Result expected(false, p1, 0);
+			auto ret = t.search(30);
+			assert(ret == expected);
+		}
+		{
+			BTree<int, 3>::Result expected(true, p3, 0);
+			auto ret = t.search(61);
+			assert(ret == expected);
+		}
+	}
+	std::cout << "All BTree tests passed\n";
+}
+
+int main(void)
+{
+	testBTreeNode();
+	testBTree();
+	return 0;
+}
---
+++ homework/ch-07 search/BTree.h
@@ -0,0 +1,276 @@
+#ifndef _BTREE_H_
+#define _BTREE_H_
+#include <vector>
+#include <utility>
+#include <limits>
+#include <type_traits>
+#include <algorithm>
+#include <iterator>
+#include <cmath>
+#include <cassert>
+#include <stdexcept>
+#include <iostream>
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+	os << "[";
+	for (size_t i = 0; i < vec.size(); ++i) {
+		os << vec[i];
+		if (i != vec.size() - 1) {
+			os << ", ";
+		}
+	}
+	os << "]";
+	return os;
+}
+
+template<typename T>
+struct BTreeNode
+{
+	struct BTreeNode *parent;
+	std::vector<T> keys;
+	std::vector<BTreeNode*> children;
+
+	BTreeNode()
+		:parent(nullptr) {}
+
+	void insertKey(int pos, const T& value)
+	{
+		if(pos < 0 || pos > static_cast<int>(keys.size()))
+		{
+			throw std::out_of_range(std::string("pos=") + std::to_string(pos) + ", keys.size()=" + std::to_string(keys.size()));
+		}
+		BTreeNode<int> n;
+		auto it = keys.begin();
+		std::advance(it, pos);
+		keys.insert(it, value);
+	}
+
+	void insertChild(int pos, BTreeNode* child)
+	{
+		if(pos < 0 || pos > static_cast<int>(children.size()))
+		{
+			throw std::out_of_range(std::string("pos=") + std::to_string(pos) + ", keys.size()=" + std::to_string(keys.size()));
+		}
+		auto it = children.begin();
+		std::advance(it, pos);
+		children.insert(it, child);
+	}
+
+	int getKeyInsertIndex(const T& value)
+	{
+		int left = 0;
+		int right = keys.size();
+
+		while (left < right)
+		{
+			int mid = left + (right - left) / 2;
+
+			if (keys[mid] < value)
+			  left = mid + 1;
+			else
+			  right = mid;
+		}
+
+		return left;
+	}
+
+	std::pair<bool, int> findKey(const T& value)
+	{
+		int left = 0;
+		int right = keys.size();
+
+		while (left < right)
+		{
+			int mid = left + (right - left) / 2;
+
+			if(keys[mid] == value)
+			{
+				return {true, mid};
+			}
+			else if (keys[mid] < value)
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				right = mid;
+			}
+		}
+
+		return {false, left};
+	}
+
+	BTreeNode*& atChildren(int index)
+	{
+		return children.at(index);
+	}
+
+	const T& atKeys(int index) const
+	{
+		return keys.at(index);
+	}
+
+	int keysSize() const
+	{
+		return static_cast<int>(keys.size());
+	}
+
+	BTreeNode* split(int mid)
+	{
+		assert(mid > 0);
+		assert(mid < keysSize());
+		
+		BTreeNode* rchild = new BTreeNode();
+		{
+			auto it = keys.begin();
+			std::advance(it, mid + 1);
+			std::copy(it, keys.end(), std::back_inserter(rchild->keys));
+			std::advance(it, -1);
+			keys.erase(it, keys.end());
+		}
+		{
+			auto it = children.begin();
+			std::advance(it, mid + 1);
+			std::copy(it, children.end(), std::back_inserter(rchild->children));
+			children.erase(it, children.end());
+		}
+
+		return rchild;
+	}
+};
+
+
+template<typename T, int m>
+class BTree
+{
+	public:
+		static_assert(m > 1, "m must greater than 1");
+		using NodeType = BTreeNode<T>;
+		using NodePtr = NodeType*;
+
+		struct Result
+		{
+			bool tag;
+			NodePtr n;
+			int i;
+			Result(bool a=false, NodePtr b=nullptr, int c=-1)
+				:tag(a), n(b), i(c){}
+			bool operator==(const Result& rhs) const
+			{
+				return tag == rhs.tag && n == rhs.n && i == rhs.i;
+			}
+
+			void display() const
+			{
+				std::cout  << "Result(tag=" << tag << ", n=" << n << ", i=" << i << ")\n";
+			}
+		};
+
+		BTree():root_(nullptr){}
+
+		Result search(const T& value);
+		Result insert(const T& value);
+
+		//only for testing
+		NodePtr& root()
+		{ return root_; }
+
+	private:
+		NodePtr root_;
+		struct BinaryNode
+		{
+			T data;
+			BinaryNode* lchild;
+			BinaryNode* rchild;
+			BinaryNode(const T& d, BinaryNode* l=nullptr, BinaryNode* r=nullptr)
+				:data(d), lchild(l), rchild(r) {}
+		};
+		Result insertUtil(T value, NodePtr n, int i);
+};
+
+
+template<typename T, int m> typename BTree<T, m>::Result BTree<T, m>::
+search(const T& value)
+{
+	NodePtr n = root_;
+	std::pair<bool, int> result = {false, 0};
+	while(n)
+	{
+		result = n->findKey(value);
+		if(result.first)
+		{
+			return {true, n, result.second};
+		}
+		else if(!n->atChildren(result.second))
+		{
+			return {false, n, result.second};
+		}
+		else
+		{
+			n = n->atChildren(result.second);
+		}
+	}
+	return {false, n, result.second};
+}
+
+
+template<typename T, int m> typename BTree<T, m>::Result BTree<T, m>::
+insert(const T& value)
+{
+	Result ret = search(value);
+	if(ret.tag)
+	{//if it exists
+		ret.tag = false;
+		return ret;
+	}
+	else
+	{
+		return insertUtil(value, ret.n, ret.i);
+	}
+}
+
+template<typename T, int m> typename BTree<T, m>::Result BTree<T, m>::
+insertUtil(T value, NodePtr n, int i)
+{
+	bool finished = false;
+	NodePtr rchild = nullptr;
+	NodePtr lchild = nullptr;
+	while(n && finished)
+	{
+		n->insertKey(i, value);
+		n->insertChild(i + 1, rchild);
+		if(n->keysSize() <= m -1)
+		{
+			finished = true;
+		}
+		else
+		{//spilt n node
+			int mid = (m - 1) / 2; 
+			value = n->keys[mid];
+			rchild = n->split(mid);
+			lchild = n;
+
+			// move mid value new node to parent
+			n = n->parent;
+			if(n)
+			{
+				i = n->getKeyInsertIndex(value);
+			}
+		}
+	}
+
+	if(!finished)
+	{//empty tree or new root
+		n = new NodeType();
+		i = 0;
+		n->insertKey(i, value);
+		n->insertChild(i, lchild);
+		n->insertChild(i + 1, rchild);
+		root_ = n;
+	}
+
+	return Result(true, n, i);
+}
+
+#endif
